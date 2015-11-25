db981c017c9d8d92349ba3ca0117784f731b2893
Álvaro Baño Fos <alvarobanofos@gmail.com>
2015-11-25 11:53:28 +0100

    initial commit

---
+++ app/Http/Controllers/ImporterController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alvarobanofos
+ * Date: 25/11/15
+ * Time: 11:38
+ */
+
+namespace App\Http\Controllers;
+
+
+
+class ImporterController extends Controller
+{
+    
+}
